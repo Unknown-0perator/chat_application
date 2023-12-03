ff8dadc1d68efa2a0f56356204f850d0e72a9d12
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-12-03 00:03:30 -0800

    LoginPage connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import ChatRoom from './pages/ChatRoom/ChatRoom';
 
 
 const App = () => {
+
+    const API_URL = process.env.REACT_APP_BACKEND_URL;
+
     return (
         <BrowserRouter>
 
             <Routes>
-                <Route path='/' element={<LoginPage />} />
+                <Route path='/' element={<LoginPage API_URL={API_URL} />} />
                 <Route path='sign-up' element={<SignUpPage />} />
                 <Route path='chat-room' element={<ChatRoom />} />
             </Routes>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,20 +1,56 @@
 import './LoginPage.scss';
 import Header from '../../components/Header/Header';
 import CTA from '../../components/CTA/CTA';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const LoginPage = () => {
+const LoginPage = ({ API_URL }) => {
+
+    const navigate = useNavigate();
+
+    const [loginCredential, setLoginCredential] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleInputChange = (event) => {
+        setLoginCredential({
+            ...loginCredential, [event.target.name]: event.target.value
+        })
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        axios.post(`${API_URL}/user/login`, {
+            username: loginCredential.username,
+            password: loginCredential.password
+        }).then((response) => {
+            if (response.status !== 403) {
+                const authToken = response.data.token;
+                sessionStorage.authToken = authToken;
+
+                navigate('/chat-room')
+            }
+        }).catch(err => alert(`Error: ${err.message}`))
+
+        event.target.reset();
+        setLoginCredential('')
+
+    }
+
     return (
         <div className="page">
             <div className="container">
                 <Header />
-                <form className="form">
+                <form onSubmit={handleFormSubmit} className="form">
                     <div className="form__group">
-                        <label htmlFor="" className="form__label">Username</label>
-                        <input type="text" className="form__input" />
+                        <label htmlFor="username" className="form__label">Username</label>
+                        <input onChange={handleInputChange} type="text" name='username' className="form__input" />
                     </div>
                     <div className="form__group">
-                        <label htmlFor="" className="form__label">Password</label>
-                        <input type="text" className="form__input" />
+                        <label htmlFor="password" className="form__label">Password</label>
+                        <input onChange={handleInputChange} type="password" name='password' className="form__input" />
                     </div>
                     <button className="form__submit">Login</button>
                 </form>
